7bdd98acaacf1c6ff12827dafa30d880e5f39905
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-05-09 13:02:01 +0900

    #3.3 Testing delete and create

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -20,6 +20,19 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('create()', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'test movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
   describe('getAll()', () => {
     it('should return an array', () => {
       const result = service.getAll();
@@ -48,5 +61,28 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe('deleteOne()', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'test movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err.message).toEqual('Movie With ID 999 Not Found');
+      }
+    });
+  });
 });
-6;
