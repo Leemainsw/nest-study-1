f4462966894d4cee4305526c616c48f5045ff1c9
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-05-09 12:01:14 +0900

    #3.2 Testing getAll and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,7 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import exp from 'constants';
+import { Movie } from './entities/Movie.entity';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -17,8 +20,33 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be 6', () => {
-    expect(2 + 4).toEqual(6);
+  describe('getAll()', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne()', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'test movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie: any = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err.message).toEqual('Movie With ID 999 Not Found');
+      }
+    });
   });
 });
 6;
