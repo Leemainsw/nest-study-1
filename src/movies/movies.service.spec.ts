b42df5d9eadd4ca268d70452e992a86a4da49543
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-05-09 11:50:41 +0900

    #3.1 Your first Unit Test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -4,6 +4,7 @@ import { MoviesService } from './movies.service';
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  // 테스트 전 실행하는 부분
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -15,4 +16,9 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be 6', () => {
+    expect(2 + 4).toEqual(6);
+  });
 });
+6;
